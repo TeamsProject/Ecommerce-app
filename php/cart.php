dbbcbdd3fbf0e08c34e6d8cfc60f55a8270afd4f
Simo9997567 <mohammedelharcha2004@outlook.com>
2024-12-15 01:25:59 +0100

    add icon-panier

--- php/cart.php
+++ php/cart.php
@@ -102,7 +102,7 @@ if (isset($_GET['delete_all'])) {
             </table>
 
             <div class="checkout-btn">
-                <a href="checkout.php" class="btn <?= ($grand_total > 1) ? '' : 'disabled'; ?>">procced to checkout</a>
+                <a href="payment.php.php" class="btn <?= ($grand_total > 1) ? '' : 'disabled'; ?>">Buy</a>
             </div>
 
         </section>
--- php/nav.php
+++ php/nav.php
@@ -29,7 +29,7 @@ session_start();
       $row_count = mysqli_num_rows($select_rows);
 
       ?>
-    <li><a class='panier' href='cart.php'>cart <span>(<?php echo $row_count; ?>)</span></a></li> 
+    <li><a class='panier' href='cart.php'><img src="../images/panier-icon.png" alt="" class="panier-icon"><span>(<?php echo $row_count; ?>)</span></a></li> 
     <li><a class="black-color" href="contact.php">contact</a></li>
 
     <li>
